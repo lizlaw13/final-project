d68d3a8d6b5483e67c7a414db7f3dcf7ea6eec9a
Liz Lawrence <jellybelly101.liz@gmail.com>
2019-02-25 12:22:01 -0800

    Modified model.py

--- server.py
+++ server.py
@@ -628,3 +628,18 @@ def show_mood(mood_id):
         user=user,
     )
 
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.autoreload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    # DebugToolbarExtension(app)
+
+    app.run(port=5000, host="0.0.0.0")
+
